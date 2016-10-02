c642f4f92f80a4c2e19409fa8fef6be0d50c300f
Kiwamu Okabe <kiwamu@debian.or.jp>
2016-10-02 14:07:52 +0900

    Why this code is verified by Infer?

--- DbC/main.c
+++ DbC/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+//#define QUEUEDEBUG /* Assert at run-time */
 #include "queue.h"
 
 #define __infer_assert(E) {  \
@@ -6,6 +8,26 @@
   if (!(E)) *s = 0xDEADBEEF; \
 }
 
+struct mylist_entry {
+	LIST_ENTRY(mylist_entry) link;
+	int val;
+};
+
+LIST_HEAD(mylist_head, mylist_entry) g_mylist_head = LIST_HEAD_INITIALIZER(mylist_head);
+
+struct mylist_entry *mylist_first(struct mylist_head *head) {
+	return LIST_FIRST(head);
+}
+
+void mylist_remove(struct mylist_entry *elm) {
+	__infer_assert(!(
+		    (elm)->link.le_next &&
+		    (elm)->link.le_next->link.le_prev !=
+		    &(elm)->link.le_next)) /* Precondition on DbC */
+	LIST_REMOVE(elm, link);
+}
 
 int main() {
+	struct mylist_entry e1;
+	mylist_remove(&e1);
 }
