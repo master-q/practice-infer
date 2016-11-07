a632e73cf400ab79dc7af6654a12261aff122b23
Kiwamu Okabe <kiwamu@debian.or.jp>
2016-11-07 21:15:24 +0900

    More

--- DbC/main.c
+++ DbC/main.c
@@ -13,21 +13,31 @@ struct mylist_entry {
 	int val;
 };
 
-LIST_HEAD(mylist_head, mylist_entry) g_mylist_head = LIST_HEAD_INITIALIZER(mylist_head);
+LIST_HEAD(mylist_head, mylist_entry) g_mylist_head =
+    LIST_HEAD_INITIALIZER(mylist_head);
 
-struct mylist_entry *mylist_first(struct mylist_head *head) {
-	return LIST_FIRST(head);
+void mylist_insert_head(struct mylist_head *head, struct mylist_entry *elm) {
+#if 1
+	__infer_assert(!(
+		    (head)->lh_first &&
+		    (head)->lh_first->link.le_prev != &(head)->lh_first
+		)) /* Precondition on DbC */
+#endif
+	LIST_INSERT_HEAD(head, elm, link);
 }
 
 void mylist_remove(struct mylist_entry *elm) {
+#if 1
 	__infer_assert(!(
 		    (elm)->link.le_next &&
-		    (elm)->link.le_next->link.le_prev !=
-		    &(elm)->link.le_next)) /* Precondition on DbC */
+		    (elm)->link.le_next->link.le_prev != &(elm)->link.le_next
+		)) /* Precondition on DbC */
+#endif
 	LIST_REMOVE(elm, link);
 }
 
 int main() {
 	struct mylist_entry e1;
+	mylist_insert_head(&g_mylist_head, &e1);
 	mylist_remove(&e1);
 }
