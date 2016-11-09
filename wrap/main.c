b0d810f3a326dda8d32ea0770e577a434b14635f
Kiwamu Okabe <kiwamu@debian.or.jp>
2016-11-09 13:03:53 +0900

    Add example to show false positive while wrap pointer

---
+++ wrap/main.c
@@ -0,0 +1,26 @@
+#include <glib.h>
+
+struct foo {
+	int *p;
+};
+
+struct foo* wrap(int *p) {
+	struct foo* f = g_malloc(sizeof(struct foo));
+	f->p = p;
+	return f;
+}
+
+struct foo* create(void) {
+	int *new_p = g_malloc(sizeof(int));
+	return wrap(new_p);
+}
+
+void delete(struct foo *f) {
+	g_free(f->p);
+	g_free(f);
+}
+
+int main() {
+	struct foo *f;
+	f = create();
+}
